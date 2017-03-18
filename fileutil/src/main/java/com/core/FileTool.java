458c668cc9b77db600014fbf8d587f68b1020d4e
“wangshuying <wangshuyingjs@163.com>
2017-03-18 23:34:22 +0800

    add new functions

--- fileutil/src/main/java/com/core/FileTool.java
+++ fileutil/src/main/java/com/core/FileTool.java
@@ -1,7 +1,19 @@
 package com.core;
 
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2017/3/18 0018.
  */
 public class FileTool {
+    public static void main(String[] args){
+
+    }
+
+
+    private static void fileCopy() throws IOException {
+        FileUtils.copyFile(new File(""),new File(""));
+    }
 }
