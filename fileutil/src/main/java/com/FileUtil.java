2dd5165cb7221cb99775725f10f2eb7410f31dc4
“wangshuying <wangshuyingjs@163.com>
2017-03-18 23:08:12 +0800

    添加了新的类

---
+++ fileutil/src/main/java/com/FileUtil.java
@@ -0,0 +1,7 @@
+package com;
+
+/**
+ * Created by Administrator on 2017/3/18 0018.
+ */
+public class FileUtil {
+}
---
+++ fileutil/src/main/java/com/core/FileTool.java
@@ -0,0 +1,7 @@
+package com.core;
+
+/**
+ * Created by Administrator on 2017/3/18 0018.
+ */
+public class FileTool {
+}
